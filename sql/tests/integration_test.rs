c33f9b2e44b3a4e2a9f8abb4c6cf958206a8e1fd
glowcouch <42631203+glowcouch@users.noreply.github.com>
2025-09-18 17:13:16 +1200

    refactor(sql): move example into integration test

--- sql/tests/integration_test.rs
+++ sql/tests/integration_test.rs
@@ -1,11 +1,25 @@
 use storage_noodle_traits::Create;
 
-#[tokio::main]
+#[tokio::test]
 async fn main() {
     // Set up the backing storage.
-    let db_pool = sqlx::sqlite::SqlitePool::connect("sqlite:./sqlite.db")
+    let db_pool = sqlx::sqlite::SqlitePool::connect("sqlite::memory:")
         .await
         .unwrap();
+
+    // Schema generation is not implemented yet
+    sqlx::query(
+        "
+            CREATE TABLE Cookie (
+            flavour TEXT,
+            Id INTEGER PRIMARY Key
+            );
+        ",
+    )
+    .execute(&db_pool)
+    .await
+    .unwrap();
+
     let backing: storage_noodle_sql::SqlBacking<_, u64> =
         storage_noodle_sql::SqlBacking::new(db_pool);
 

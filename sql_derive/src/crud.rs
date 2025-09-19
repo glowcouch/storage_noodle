glowcouch/storage_noodle

commit 62c745719f7047d3010eae4e1671e6795dc6ea70

refactor(sql/derive): seperate parts of Create derive out

sql_derive/src/crud.rs
@@ -25,36 +25,8 @@ fn create_impl(item: syn::ItemStruct, backing_db: syn::Type, raw_id: syn::Type) 
     // The table name.
     let table = ident.to_string();
 
-    // List of field idents.
-    let fields = fields.iter().enumerate().map(|(index, field)| {
-        // Fall back to the index if the field has no name (unit structs).
-        if let Some(ident) = &field.ident {
-            ident.to_owned()
-        } else {
-            syn::Ident::new(&index.to_string(), proc_macro2::Span::mixed_site())
-        }
-    });
-
-    // List of column names.
-    let columns = fields
-        .clone()
-        .map(|ident| ident.to_string())
-        .collect::<Vec<_>>();
-
-    // List of "?" to be filled in at runtime.
-    let values = (0..columns.len())
-        .map(|_| "?")
-        .collect::<Vec<_>>()
-        .join(", ");
-
-    // List of `.bind()` calls to run on the query.
-    let bind_calls: TokenStream = fields
-        .map(|ident| {
-            quote! {
-                .bind(&self.#ident)
-            }
-        })
-        .collect();
+    // List of columns.
+    let columns = crate::sql::Column::from_fields(fields.clone());
 
     // The SQL query to run.
     let query = {
@@ -65,13 +37,29 @@ fn create_impl(item: syn::ItemStruct, backing_db: syn::Type, raw_id: syn::Type) 
                 RETURNING {};
             ",
             table,
-            columns.join(", "),
-            values,
+            columns
+                .iter()
+                .map(|c| c.name.clone())
+                .collect::<Vec<_>>()
+                .join(", "), // List of column names (in order).
+            (0..columns.len())
+                .map(|_| "?")
+                .collect::<Vec<_>>()
+                .join(", "), // List of "?" - to be filled in with bind calls.
             crate::sql::ID_FIELD_NAME,
         );
         syn::LitStr::new(&query, proc_macro2::Span::mixed_site())
     };
 
+    // List of `.bind()` calls to run on the query.
+    let bind_calls: TokenStream = fields
+        .iter()
+        .map(|ident| {
+            quote! {.bind(&self.#ident)}
+        })
+        .collect();
+
+    // Implement the trait.
     quote! {
         impl ::storage_noodle_sql::macro_helpers::Create<::storage_noodle_sql::SqlBacking<#backing_db, #raw_id>> for #ident
         {

sql_derive/src/sql.rs
@@ -2,6 +2,38 @@
 //!
 //! Table names are copied directly from the struct name.
 //!
+//! Column names are copied directly from the field name.
+//!
 //! The Id field is defined by [`ID_FIELD_NAME`].
 
 pub const ID_FIELD_NAME: &str = "Id";
+
+/// Describes a SQL column.
+pub struct Column {
+    /// The column name.
+    pub name: String,
+
+    /// The struct field corrosponding to the column.
+    pub ident: syn::Ident,
+}
+
+impl Column {
+    /// Creates a list of [`Column`]s from a [`syn::Fields`].
+    pub fn from_fields(value: syn::Fields) -> Vec<Self> {
+        value
+            .iter()
+            .enumerate()
+            .map(|(i, field)| {
+                // Fall back to the index if the field has no name (unit structs).
+                let ident = field.ident.clone().unwrap_or_else(|| {
+                    syn::Ident::new(&i.to_string(), proc_macro2::Span::mixed_site())
+                });
+
+                Column {
+                    name: ident.to_string(),
+                    ident,
+                }
+            })
+            .collect()
+    }
+}

commit 72b151a555dc431dce8bfd06a7fe18ed8dc450b0

fix(sql/derive): fix error in bind section of Create macro
fixes bug introduced in 62c745719f7047d3010eae4e1671e6795dc6ea70

sql_derive/src/crud.rs
@@ -75,10 +75,11 @@ fn create_impl(item: syn::ItemStruct, backing_db: syn::Type, raw_id: syn::Type) 
     };
 
     // List of `.bind()` calls to run on the query.
-    let bind_calls: TokenStream = fields
+    let bind_calls: TokenStream = columns
         .iter()
-        .map(|ident| {
-            quote! {.bind(&self.#ident)}
+        .map(|column| {
+            let field = &column.ident;
+            quote! {.bind(&self.#field)}
         })
         .collect();
 
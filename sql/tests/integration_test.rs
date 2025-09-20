7811a192ac9450cd551244756d3b1e05ca3c10aa
glowcouch <42631203+glowcouch@users.noreply.github.com>
2025-09-20 13:11:33 +1200

    test(sql): update integration test to include Read trait

--- sql/tests/integration_test.rs
+++ sql/tests/integration_test.rs
@@ -1,4 +1,4 @@
-use storage_noodle_traits::Create;
+use storage_noodle_traits::{Create, Read};
 
 #[tokio::test]
 async fn main() {
@@ -23,16 +23,25 @@ async fn main() {
     let backing: storage_noodle_sql::SqlBacking<_, u32> =
         storage_noodle_sql::SqlBacking::new(db_pool);
 
-    // Create a new record.
-    Cookie {
+    // Create a cookie
+    let cookie = Cookie {
         flavour: "chocolate".to_string(),
-    }
+    };
+
+    // Create a new record.
+    let cookie_id = cookie
     .create(&backing)
     .await
     .unwrap();
+
+    // Read the record back from the db.
+    let returned_cookie = Cookie::read(&backing, &cookie_id).await.unwrap();
+
+    // Check that the cookie did not get altered.
+    assert_eq!(returned_cookie, cookie);
 }
 
-#[derive(storage_noodle_sql::Create, storage_noodle_sql::Read, sqlx::FromRow)]
+#[derive(Debug, PartialEq, storage_noodle_sql::Create, storage_noodle_sql::Read, sqlx::FromRow)]
 #[config_noodle_sql(sqlx::sqlite::Sqlite, u32)]
 struct Cookie {
     flavour: String,
